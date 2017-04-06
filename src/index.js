eb60bf0a89a3176310d7950964b238ba961e6bea
Jeremy Morrell <jmorrell@heroku.com>
2017-04-05 21:53:20 -0600

    Added throng

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 #!/usr/bin/env node
 
-const http = require('http')
+const http = require('http');
+const throng = require('throng');
 
-const PORT = process.env.PORT || 5000
+const workers = process.env.WEB_CONCURRENCY || 4;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/error') {
-    throw new Error('Oh no!');
-  }
+throng(workers, startWorker);
 
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(`Hello, world!\n`)
-})
+function startWorker() {
+  const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(`Listening on ${PORT}`))
+  const server = http.createServer((req, res) => {
+    if (req.url === '/error') {
+      throw new Error('Oh no!');
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`Hello, world!\n`);
+  });
+
+  server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+}
+
