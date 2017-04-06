dda0a6ab6e53318655fa74e8358a1e7ba5cba5cc
Jeremy Morrell <jmorrell@heroku.com>
2017-04-05 21:42:40 -0600

    Made crashy route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const http = require('http')
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/error') {
+    throw new Error('Oh no!');
+  }
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end(`Hello, world!\n`)
